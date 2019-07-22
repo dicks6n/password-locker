61a9a3e69060fb510d1f4676984ce46c7fd4bb8a
dicks6n <dikkymccria@gmail.com>
2019-07-22 00:07:18 -0700

    Function to generate a password

--- lock.py
+++ lock.py
@@ -1,5 +1,5 @@
 import pyperclip
-from user_credentials import User, Credential
+
 
 def create_user(fname,lname,password):
 	'''
@@ -7,9 +7,21 @@ def create_user(fname,lname,password):
 	'''
 	new_user = User(fname,lname,password)
 	return new_user
-
-    def save_user(user):
+def save_user(user):
 	'''
 	Function to save a new user account
 	'''
 	User.save_user(user)
+
+def verify_user(first_name,password):
+	'''
+	Function that verifies the existance of the user before creating credentials
+	'''
+	checking_user = Credential.check_user(first_name,password)
+	return checking_user
+def generate_password():
+	'''
+	Function to generate a password automatically
+	'''
+	gen_pass = Credential.generate_password()
+	return gen_pass
